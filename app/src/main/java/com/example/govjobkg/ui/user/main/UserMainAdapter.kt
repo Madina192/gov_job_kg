4e716a1ea3864fc99f08bc2e70cea46f216fba44
Madina <madinaakpaeva000@gmail.com>
2024-11-09 02:59:37 +0600

    added categories

---
+++ app/src/main/java/com/example/govjobkg/ui/user/main/UserMainAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.govjobkg.ui.user.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.govjobkg.databinding.ItemCategoryBinding
+import com.example.govjobkg.network.models.Category
+
+class UserMainAdapter : Adapter<UserMainAdapter.UserMainViewHolder>(){
+
+    private var list = listOf<Category>()
+
+    fun setList(categories: List<Category>) {
+        list = categories
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserMainViewHolder {
+        return UserMainViewHolder(ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: UserMainViewHolder, position: Int) {
+        holder.onBind(list[position])
+    }
+
+    inner class UserMainViewHolder(private val binding : ItemCategoryBinding) : ViewHolder(binding.root){
+        fun onBind(category: Category){
+            binding.ivIcon.setImageResource(category.image)
+            binding.tvTitle.text = category.title
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/govjobkg/ui/user/main/UserMainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.govjobkg.ui.user.main
+
+import com.example.govjobkg.base.BaseViewModel
+
+class UserMainViewModel : BaseViewModel() {
+}
