e60eecf0ef7d79a445869a7868f731fb7cd76631
Madina <madinaakpaeva000@gmail.com>
2024-11-09 04:44:42 +0600

    added vacancies screen

---
+++ app/src/main/java/com/example/govjobkg/network/models/Vacancy.kt
@@ -0,0 +1,9 @@
+package com.example.govjobkg.network.models
+
+data class Vacancy(
+    val jobTitle : String,
+    val salary : String,
+    val company : String,
+    val experience : String,
+    val description : String
+)
